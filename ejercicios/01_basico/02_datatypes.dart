5376cb9a390e1df822b3c453efc0e047a301eaa7
Jhoss <kuro.rc10@gmail.com>
2020-03-26 16:26:56 -0500

    Trabajando con cadenas

--- ejercicios/01_basico/02_datatypes.dart
+++ ejercicios/01_basico/02_datatypes.dart
@@ -7,9 +7,24 @@ main() {
 
   int x = 10,y = 20,z = 30;
 
-  print(x);
+/*   print(x);
   print(y);
   print(z);
   print(c);
+ */
+
+  // Strings - Cadena de caracteres
+
+  String student = 'Jose';
+  String teacher = "Fernando" + " Herrera";
+  String director = 'O\'Connor';
+
+  print(director);
+
+  String message = '''Aprendiendo Dart!!
+Aprendiendo ReactiveX !!
+Con el profesor $teacher''';
+
+  print(message);
 
 }
