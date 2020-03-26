e8bde401e8cd5bd9e30df79735c648c18e7e2f34
Jhoss <kuro.rc10@gmail.com>
2020-03-26 15:28:03 -0500

    Trabajando con números

--- ejercicios/01_basico/02_datatypes.dart
+++ ejercicios/01_basico/02_datatypes.dart
@@ -1,7 +1,15 @@
 main() {
-  // Números
-  var a = 10;
-  a = 20;
-  print(a);
+  // ======= Números =======
+
+  int a = 10;
+  double b = 5.5;
+  int c; // null
+
+  int x = 10,y = 20,z = 30;
+
+  print(x);
+  print(y);
+  print(z);
+  print(c);
 
 }
