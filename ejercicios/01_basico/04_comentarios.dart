721e85ded791b23ea984db00fae2273a02b57792
Jhoss <kuro.rc10@gmail.com>
2020-03-27 11:40:25 -0500

    Comentarios

---
+++ ejercicios/01_basico/04_comentarios.dart
@@ -0,0 +1,33 @@
+/**
+  * Este es un archivo para explicar los comentarios
+  *
+  * [creadoEl] : 27/03/20
+  *
+ */
+
+main() {
+
+  // Nombres
+  final peoples = [
+    // Esta es la primera persona
+    'Fernando', // [0]
+
+    // Esta es la segunda persona
+    'Jose', // [1]
+
+    // Esta es la tercera persona
+    'Luis', // [2]
+  ];
+
+  /* 
+    Los comentarios de multilínea son útiles cuando 
+    trabajamos con bloques de código
+    #NuncaParesDeAprender
+  
+   */
+
+}
+
+
+/// Comentarios para evitar que sean ignorados por herramienta como dartdoc
+/// [aprendiendoCon] : Fernando Herrera
