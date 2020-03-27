49df351604f8dbcebc5ecfcb385f524b6253f12b
Jhoss <kuro.rc10@gmail.com>
2020-03-27 06:26:55 -0500

    Trabajando con listas

--- ejercicios/01_basico/02_datatypes.dart
+++ ejercicios/01_basico/02_datatypes.dart
@@ -27,12 +27,39 @@ Con el profesor $teacher''';
 
   print(message);
 
-  // Booleans
+  // ======= Booleans =======
 
   bool isActive = true;
   bool isRunning = false;
 
   print(isActive);
   print(isRunning);
+
+  // Listas
+  var myList = ['Superman',100 , 'Batman',70 , true];  
+  List<Object> otherList = ['Hello', 'World', 10]; 
+
+  List<String> dinamicList = new List(); // dinámico
+  dinamicList.add('A');
+  dinamicList.add('B');
+  dinamicList.add('C');
+
+  List<int> staticList = new List(5); // estático
+  staticList[0] = 10;
+  staticList[1] = 20;
+  staticList[2] = 30;
+  staticList[3] = 40;
+  staticList[4] = 50;
+
+  print(staticList);
+
+  List<String> heroes = new List();
+    heroes.addAll(['Ironman', 'Falcon', 'Hulk']);
+
+  List<String> villains = new List();
+    villains..add('Thanos')
+            ..add('Red Skull')
+            ..add('Doom');
+
 
 }
