c5ad7df6990ea0a8b03791c7616acd1d79fc056d
Jhoss <kuro.rc10@gmail.com>
2020-03-27 13:47:04 -0500

    Operadores aritméticos

---
+++ ejercicios/01_basico/05_operadores_mat.dart
@@ -0,0 +1,28 @@
+/**
+ *    Un operador es un símbolo que le dice al compilador
+ *    qué debe realizar una tarea
+ *    matemática, relacional o lógica
+ *    y debe de producir un resultado
+ * 
+ */
+
+main() {
+
+  int a;
+  a = 10 + 5;   // Adición
+  a = 20 - 10;  // Sustración
+  a = 10 * 2;   // Producto
+
+  double b;
+  b = 10 / 2 ;   // División
+  b = 10.0 % 3 ; // Modulo (Resto)
+  b = -b;        // Negación
+
+  int c;
+  c = 10 ~/ 3;   // Cociente ( Solo la parte entera )
+
+  int d = 1;     // Asignación
+
+  d++ ;           // Post Incremento +1
+  d-- ;           // Post Decremento -1
+}
