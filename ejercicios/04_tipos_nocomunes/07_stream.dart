5cabe888025d0ae9b00fc98904919ca12a3acad6
Jhoss <kuro.rc10@gmail.com>
2020-04-07 14:23:34 -0500

    onError, onDone, cancelOnError

--- ejercicios/04_tipos_nocomunes/07_stream.dart
+++ ejercicios/04_tipos_nocomunes/07_stream.dart
@@ -4,17 +4,35 @@ main() {
   
   final streamController = StreamController();
 
-  streamController.stream.listen((data) {
-    print('Despegando! $data');
-  });
+  streamController.stream.listen(
+    (data) => print('Despegando! $data'),
+    onError: (err) => print('Error! $err'),
+    onDone: ()=> print('Mision completa!'),
+    cancelOnError: false
+  );
 
   streamController.sink.add('Apollo 11');
+  streamController.sink.add('Apollo 12');
+  streamController.sink.add('Apollo 13');
+  streamController.sink.add('Apollo 14');
+  streamController.sink.addError('Houston, Tenemos un problema!');
+  streamController.sink.add('Apollo 15');
+
+
+  streamController.sink.close();
+
 
   print('Fin del main');
 
   /**
    * Fin del main
-   * Despegando Apollo 11
+   * Despegando! Apollo 11
+   * Despegando! Apollo 12
+   * Despegando! Apollo 13
+   * Despegando! Apollo 14
+   * Error Houston, Tenemos un problema!
+   * Despegando! Apollo 15
+   * Misión completa
    */
 
 }
