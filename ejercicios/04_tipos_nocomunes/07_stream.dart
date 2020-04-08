8e4a216b0a2a8e6c0d49be7d4767f109bffbb67e
Jhoss <kuro.rc10@gmail.com>
2020-04-07 19:28:00 -0500

    Uso del broadcast de un Stream

--- ejercicios/04_tipos_nocomunes/07_stream.dart
+++ ejercicios/04_tipos_nocomunes/07_stream.dart
@@ -2,21 +2,25 @@ import 'dart:async';
 
 main() {
   
-  final streamController = StreamController();
+  final streamController = new StreamController<String>.broadcast();
 
   streamController.stream.listen(
     (data) => print('Despegando! $data'),
     onError: (err) => print('Error! $err'),
     onDone: ()=> print('Mision completa!'),
-    cancelOnError: false
+    cancelOnError: true
+  );
+
+  streamController.stream.listen(
+    (data) => print('Despegando Stream 2! $data'),
+    onError: (err) => print('Error Stream 2! $err'),
+    onDone: ()=> print('Mision completa Stream 2!'),
+    cancelOnError: true
   );
 
   streamController.sink.add('Apollo 11');
-  streamController.sink.add('Apollo 12');
-  streamController.sink.add('Apollo 13');
-  streamController.sink.add('Apollo 14');
   streamController.sink.addError('Houston, Tenemos un problema!');
-  streamController.sink.add('Apollo 15');
+  streamController.sink.add('Apollo 12');
 
 
   streamController.sink.close();
@@ -27,12 +31,8 @@ main() {
   /**
    * Fin del main
    * Despegando! Apollo 11
-   * Despegando! Apollo 12
-   * Despegando! Apollo 13
-   * Despegando! Apollo 14
+   * Despegando Stream 2! Apollo 11
    * Error Houston, Tenemos un problema!
-   * Despegando! Apollo 15
-   * Misión completa
+   * Error Stream 2!,Houston Tenemos un problema!
    */
-
 }
