9dc8036bafc0110a2681f7e9b4cd4ad928a76674
Jhoss <kuro.rc10@gmail.com>
2020-04-24 16:48:08 -0500

    Usando getters y setters

--- ejercicios/05_clases/01_clase.dart
+++ ejercicios/05_clases/01_clase.dart
@@ -1,12 +1,15 @@
 import 'clases/People.dart';
 
-main() {
+void main() {
  
-  final p1 = new People();
+  final p1 = new People(); 
+
+  p1..name = 'José'
+    ..age  = 21
+    ..bio  = 'soy de Perú';
 
 
-
-  print(p1);
+  print('Mi nombre es ' + p1.name + ' y ' + p1.bio);
 }
 
 
--- ejercicios/05_clases/clases/People.dart
+++ ejercicios/05_clases/clases/People.dart
@@ -1,10 +1,33 @@
 class People {
-  // Campos o propiedades
+  /* Campos o propiedades */
   String _name;
   int _age;
   String _bio;
 
-  // Metodos
+  /* Get y sets */
+  String get name{
+    return _name;
+  } // Como función
+
+  void set name(String name){
+    _name = name;
+  }
+
+  int get age => _age; // Función de flecha
+
+  void set age(int age){
+    _age = age;
+  }
+
+  String get bio{
+    return _bio;
+  }
+
+  void set bio(String bio){
+    _bio = bio;
+  }
+
+  /* Metodos */
   @override
   String toString() => '$_name $_age $_bio';
 }
