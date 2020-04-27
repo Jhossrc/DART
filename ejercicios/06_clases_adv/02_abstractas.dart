ab3fb205e5b73fca893d2ec0f7b299267a9f11e3
Jhoss <kuro.rc10@gmail.com>
2020-04-27 10:26:17 -0500

    Clases abstractas

---
+++ ejercicios/06_clases_adv/02_abstractas.dart
@@ -0,0 +1,36 @@
+abstract class Vehiculo {
+
+  bool encendido = false;
+
+  void encender() {
+    encendido = true;
+    print('Vehículo encendido');
+  }
+
+  void apagar() {
+    encendido = false;
+    print('Vehículo apagado');
+  }
+
+  bool revisarMotor();
+}
+
+class Carro extends Vehiculo {
+  int km = 0;
+
+  @override
+  bool revisarMotor() {
+    print('Motor OK!');
+    return true;
+  }
+  
+}
+
+main() {
+  
+  final ford = new Carro();
+  ford.encender(); // Vehiculo encendido
+
+  ford.revisarMotor();
+
+}
