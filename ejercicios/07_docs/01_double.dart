6235b35dbf721c1184858353533b65993ec0688b
Jhoss <kuro.rc10@gmail.com>
2020-04-27 12:25:43 -0500

    Doubles

---
+++ ejercicios/07_docs/01_double.dart
@@ -0,0 +1,41 @@
+main() {
+  
+  double number = 3.1416;
+  double infinite = double.infinity;
+
+  // toString() : Convierte un objeto a string
+  print('Firma : ${number.toString()}');
+
+  // sign : Retorna la firma
+  print('Firma : ${number.sign} :: $number');
+  // Firma : 1.0 :: 3.1416
+
+  // isFinite : Retorna verdadero si es finito.
+  print('isFinite : ${number.isFinite} :: $number');
+  // isFinite : true :: 3.1416
+  print('isFinite : ${infinite.isFinite} :: $infinite');
+  // isFinite : false :: Infinity
+
+  // abs() : Retorna el valor absoluto del número
+  print('abs : ${(number * -1).abs()} :: $number');
+  // abs : 3.1416 :: 3.1416
+
+  // ceil() : Redondea al numero más cercano (int).
+  print('ceil : ${number.ceil()} :: $number');
+  // ceil : 4 :: 3.1416
+
+  // ceilToDouble() : Redondea al numero más cercano (double).
+  print('ceilToDouble : ${number.ceilToDouble()} :: $number');
+  // ceilToDouble : 4.0 :: 3.1416
+
+  // round() : Redondea un número matemáticamente
+  print('round : ${number.round()} :: $number');
+  // round : 3 :: 3.1416
+
+  // Retorna un número que se encuentre en el rango que sea menor o igual número. Si es menor a 0 el izquierdo
+  print('clamp : ${number.clamp(1, 2)} :: $number');
+  // clamp : 2 :: 3.1416
+
+
+
+}
